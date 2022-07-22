dbe09c93683e875d80c152954431b900e16b75e7
Sophie Koonin <sophie@localghost.dev>
2022-07-22 19:14:17 +0100

    Update to use eleventy-generated CSS, and add a template

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 // Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const debugFilter = require('./src/filters/debug');
 
 module.exports = (config) => {
   config.addWatchTarget('./src');
 
   config.addPassthroughCopy({ './src/static': '/' });
+
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+  config.addFilter('debug', debugFilter);
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
